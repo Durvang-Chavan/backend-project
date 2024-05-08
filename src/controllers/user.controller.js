2f3288e06cb8f23c833ce805757fd9713c69c764
Durvang-Chavan <durvangchavangithub@gmail.com>
2024-05-08 23:04:55 +0530

    add code for user's controller and route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,27 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js"
+import { User } from "../models/user.model.js";
 const registUser = asyncHandler(  async (req, res)=>{
-    res.status(200).json({
-        message: "WELL DONE DURVANG"
+    //getting data from frontend
+    const {fullname, email, username, password } = req.body 
+    // validation 
+    if (
+        [fullname, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    //checking if user exists in database
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
     })
-});
+
+    if (existedUser) {
+        throw new ApiError(409, "the user with same username and email already exists")
+    }
+
+    })
+    
+
 
 export { registUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express"
 import { registUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router()
-router.route("/register").post(registUser);
+//injecting multer middleware in routes for file upload
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registUser
+);
 export default router
