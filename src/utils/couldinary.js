6f0039366cd594da322ce1e7576296c42fee1817
Durvang-Chavan <durvangchavangithub@gmail.com>
2024-05-10 22:46:44 +0530

    modified the code

--- src/utils/couldinary.js
+++ src/utils/couldinary.js
@@ -15,10 +15,11 @@ const uploadOnCloudinary = async (localFilePath)=>{
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type:'auto'
         });
-        console.log("file is uploaded on cloudinary! ", response.url);
+        // console.log("file is uploaded on cloudinary! ", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
-        fs.unlink(localFilePath) //removing the file stored in server as the upload operation failed.
+        fs.unlinkSync(localFilePath) //removing the file stored in server as the upload operation failed.
         return null
     }
 }
